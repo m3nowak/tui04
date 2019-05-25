6517b9529674cbdbc94427b9559a89ad116d047e
Mikołaj Nowak <miknowak@outlook.com>
2019-05-25 23:48:37 +0200

    końcowe wartości klasyfikatora

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import classifier
 def main():
     ds_x, ds_y = dataset.load_dataset()
     clsfr = classifier.create_classifier(verbose=True)
-    classifier.train(clsfr, ds_x[:300], ds_y[:300])
-    print(classifier.rate(clsfr, ds_x[300:400], ds_y[300:400]))
-    classifier.dump_classifier(clsfr, '2')
+    classifier.train(clsfr, ds_x[:9000], ds_y[:9000])
+    print(classifier.rate(clsfr, ds_x[9000:], ds_y[9000:]))
+    classifier.dump_classifier(clsfr)
 
 if __name__ == "__main__":
     main()
